ed69271e2ca3240fc8d3e027fa8b24722a01215d
Juwon Park <devprofessionalism@gmail.com>
2022-07-31 18:48:28 +0900

    Hello 서블릿

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,10 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
+//모든 컴포넌트를 찾아서 서블릿에 자동 등록
 @SpringBootApplication
 public class ServletApplication {
 
