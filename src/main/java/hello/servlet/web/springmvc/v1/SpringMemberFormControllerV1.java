78f7c255d70c49da709163e33aeee3242fd69589
juwon <58173061+juwon0605@users.noreply.github.com>
2022-08-04 02:10:19 +0900

    스프링 MVC - 컨트롤러 통합

--- src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class SpringMemberFormControllerV1 {
 
-	@RequestMapping("/springmvc/v1/members/new-from")
+	@RequestMapping("/springmvc/v1/members/new-form")
 	public ModelAndView process() {
 		return new ModelAndView("new-form");
 	}
